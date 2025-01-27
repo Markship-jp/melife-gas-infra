1116395b60a149a735c34e2cd29850bb9f6190f0
Markship-jp <koh.watanabe@markship.jp>
2025-01-27 19:53:22 +0900

    parameter追加

--- terraform/dev/parameterstore.tf
+++ terraform/dev/parameterstore.tf
@@ -82,3 +82,116 @@ resource "aws_ssm_parameter" "ZIPCODE_APIKEY" {
     ignore_changes = [value]
   }
 }
+
+
+resource "aws_ssm_parameter" "PAYGENT_MERCHANT_ID" {
+  name  = "${local.parameterstore_path}/PAYGENT_MERCHANT_ID"
+  type  = "SecureString"
+  value = "PLACEHOLDER" # セキュアストリングの値はコンソールに設定するため仮の値を設定
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "PAYGENT_MERCHANT_NAME" {
+  name  = "${local.parameterstore_path}/PAYGENT_MERCHANT_NAME"
+  type  = "SecureString"
+  value = "PLACEHOLDER" # セキュアストリングの値はコンソールに設定するため仮の値を設定
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "PAYGENT_HASH_KEY" {
+  name  = "${local.parameterstore_path}/PAYGENT_HASH_KEY"
+  type  = "SecureString"
+  value = "PLACEHOLDER" # セキュアストリングの値はコンソールに設定するため仮の値を設定
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "PAYGENT_COMPANY_NAME" {
+  name  = "${local.parameterstore_path}/PAYGENT_COMPANY_NAME"
+  type  = "SecureString"
+  value = "PLACEHOLDER" # セキュアストリングの値はコンソールに設定するため仮の値を設定
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "PAYGENT_LINK_URL" {
+  name  = "${local.parameterstore_path}/PAYGENT_LINK_URL"
+  type  = "SecureString"
+  value = "PLACEHOLDER" # セキュアストリングの値はコンソールに設定するため仮の値を設定
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "PAYGENT_CONNECT_ID" {
+  name  = "${local.parameterstore_path}/PAYGENT_CONNECT_ID"
+  type  = "SecureString"
+  value = "PLACEHOLDER" # セキュアストリングの値はコンソールに設定するため仮の値を設定
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "PAYGENT_CONNECT_PASSWORD" {
+  name  = "${local.parameterstore_path}/PAYGENT_CONNECT_PASSWORD"
+  type  = "SecureString"
+  value = "PLACEHOLDER" # セキュアストリングの値はコンソールに設定するため仮の値を設定
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "PAYGENT_CONNECT_VERSION" {
+  name  = "${local.parameterstore_path}/PAYGENT_CONNECT_VERSION"
+  type  = "SecureString"
+  value = "PLACEHOLDER" # セキュアストリングの値はコンソールに設定するため仮の値を設定
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+
+resource "aws_ssm_parameter" "PAYGENT_PFX_PASSWORD" {
+  name  = "${local.parameterstore_path}/PAYGENT_PFX_PASSWORD"
+  type  = "SecureString"
+  value = "PLACEHOLDER" # セキュアストリングの値はコンソールに設定するため仮の値を設定
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+
+resource "aws_ssm_parameter" "PAYGENT_PFX_KEY" {
+  name  = "${local.parameterstore_path}/PAYGENT_PFX_KEY"
+  type  = "SecureString"
+  value = "PLACEHOLDER" # セキュアストリングの値はコンソールに設定するため仮の値を設定
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+
+resource "aws_ssm_parameter" "PAYGENT_MODULE_CONNECT_URL" {
+  name  = "${local.parameterstore_path}/PAYGENT_MODULE_CONNECT_URL"
+  type  = "SecureString"
+  value = "PLACEHOLDER" # セキュアストリングの値はコンソールに設定するため仮の値を設定
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+
+resource "aws_ssm_parameter" "AWS_DOWNLOAD_BUCKET_NAME" {
+  name  = "${local.parameterstore_path}/AWS_DOWNLOAD_BUCKET_NAME"
+  type  = "SecureString"
+  value = "PLACEHOLDER" # セキュアストリングの値はコンソールに設定するため仮の値を設定
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
