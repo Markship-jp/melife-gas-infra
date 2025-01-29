597d5235d998885fc31a7ef5beeaeff3cef1e083
Markship-jp <koh.watanabe@markship.jp>
2025-01-29 10:14:37 +0900

    IPアドレス追加

--- terraform/dev/waf.tf
+++ terraform/dev/waf.tf
@@ -7,7 +7,9 @@ locals {
     "150.195.208.121/32",
     "140.82.207.200/32",
     "210.170.191.42/32",
-    "153.242.120.136/32"
+    "153.242.120.136/32",
+    "101.102.168.242/32",
+    "210.136.82.2/32"
   ]
   api_webacl_name        = "${var.env}-${var.project}-api-webacl"
   cloudfront_webacl_name = "${var.env}-${var.project}-cloudfront-webacl"
